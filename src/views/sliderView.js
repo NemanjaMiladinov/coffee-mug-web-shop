812c9973786e698c4036e4aee3a605402a5c06e3
Nemanja Miladinov <nemchytheking@gmail.com>
2023-09-07 15:29:08 +0200

    fixed minor issue

--- src/views/sliderView.js
+++ src/views/sliderView.js
@@ -7,12 +7,13 @@ class Slider {
 
   sliderButtons = document.querySelectorAll(".slider-btn");
 
-  initialPosition() {
-    for (let i = 0; i < this.sliderProducts.length; i++) {
-      this.sliderProducts[i].style.transform = `translateX(${i * 330}px)`;
-      this.sliderProducts[i].style.transition = "all 1s";
+  initialPosition(products) {
+    for (let i = 0; i < products.length; i++) {
+      products[i].style.transform = `translateX(${i * 330}px)`;
+      products[i].style.transition = "all 1s";
     }
   }
+
   eventhandler(next, before) {
     this.sliderButtons.forEach((btn) => {
       btn.addEventListener("click", (e) => {
@@ -30,25 +31,9 @@ class Slider {
         }
       });
     });
-
-    // this.next.addEventListener("click", (e) => {
-    //   console.log(e);
-    //   if (e.target.textContent.includes("navigate_next")) {
-    //     console.log("move slider next");
-    //     next();
-    //     return;
-    //   }
-    //   if (e.target.textContent.includes("navigate_before")) {
-    //     console.log("move slider before");
-    //     before();
-    //     return;
-    //   }
-    //   // handler();
-    // });
   }
-
-  getQuery() {
-    return this.sliderProducts;
+  getQuery(products) {
+    return products;
   }
 }
 export default new Slider();
